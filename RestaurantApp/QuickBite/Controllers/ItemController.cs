d8bf756d388d3ed2c723a5b53542764dbddedbdb
Zarmina Desktop <zarminaehsan@icloud.com>
2025-08-06 14:45:35 +0200

    Add Grid View and update ItemController accordingly Add ItemListViewModel to wrap data for the View

--- RestaurantApp/QuickBite/Controllers/ItemController.cs
+++ RestaurantApp/QuickBite/Controllers/ItemController.cs
@@ -1,33 +1,186 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using QuickBite.Models;
+using QuickBite.ViewModels;
 
-namespace QuickBite.Controllers
+
+namespace QuickBite.Controllers;
+
+public class ItemController : Controller
 {
-    public class ItemController : Controller
+    public IActionResult Table()
     {
-        public IActionResult Table()
+        var items = GetItems();
+        var itemListViewModel = new ItemListViewModel(items, "Table");
+        return View(itemListViewModel);
+    }
+
+    public IActionResult Grid()
+    {
+        var items = GetItems();
+        var itemListViewModel = new ItemListViewModel(items, "Grid");
+        return View(itemListViewModel);
+    }
+
+    public List<Item> GetItems()
+    {
+        var items = new List<Item>();
+
+        var item1 = new Item
         {
-            var items = new List<Item>();
-            
-            var item1 = new Item();
-            item1.ItemId = 1;
-            item1.Name = "Pizza";
-            item1.Price = 60;
+            ItemId = 1,
+            Name = "Pizza",
+            Price = 60,
+            Description = "Delicious Italian dish with a thin crust topped with tomato sauce, cheese, and various toppings.",
+            ImageUrl = "/images/vecteezy_pizza-hawaiian-cheese.jpg"
+        };
 
-            //another way to do the same as above, but this is preferable
-            var item2 = new Item
-            {
-                ItemId = 2,
-                Name = "Fried Chicken Leg",
-                Price = 15
-            };
 
-            items.Add(item1);
-            items.Add(item2);
+        //another way to do the same as above, but this is preferable
+        var item2 = new Item
+        {
+            ItemId = 2,
+            Name = "Fried Chicken Leg",
+            Price = 160,
+            Description = "Delicious .....",
+            ImageUrl = "/images/vecteezy_fried-chicken-leg.jpg"
+        };
 
-            ViewBag.CurrentViewName = "List of Shop Items";
-            return View(items);
-        }
+        var item3 = new Item
+        {
+            ItemId = 3,
+            Name = "Ribs",
+            Price = 260,
+            Description = "Delicious .....",
+            ImageUrl = "/images/ribs.jpg"
+        };
+
+        var item4 = new Item
+        {
+            ItemId = 4,
+            Name = "Fries",
+            Price = 80,
+            Description = "Delicious .....",
+            ImageUrl = "/images/fries.jpg"
+        };
+
+        var item5 = new Item
+        {
+            ItemId = 5,
+            Name = "Fish and Chips",
+            Price = 180,
+            Description = "Delicious .....",
+            ImageUrl = "/images/fish and chips.jpg"
+        };
+
+        var item6 = new Item
+        {
+            ItemId = 6,
+            Name = "Tacos",
+            Price = 180,
+            Description = "Delicious .....",
+            ImageUrl = "/images/vecteezy_ai-generated-tacos.jpg"
+        };
+
+        items.Add(item1);
+        items.Add(item2);
+        items.Add(item3);
+        items.Add(item4);
+        items.Add(item5);
+        items.Add(item6);
+
+        ViewBag.CurrentViewName = "List of Shop Items";
+        return items;
     }
 }
 
+
+/*
+public class ItemController : Controller
+{
+    public IActionResult Table()
+    {
+        var items = GetItems();
+        ViewBag.CurrentViewName = "Table";
+        return View(items);
+    }
+
+    public IActionResult Grid()
+    {
+        var items = GetItems();
+        ViewBag.CurrentViewName = "Grid";
+        return View(items);
+    }
+    public List<Item> GetItems()
+    {
+        var items = new List<Item>();
+
+        var item1 = new Item
+        {
+            ItemId = 1,
+            Name = "Pizza",
+            Price = 60,
+            Description = "Delicious Italian dish with a thin crust topped with tomato sauce, cheese, and various toppings.",
+            ImageUrl = "/images/vecteezy_pizza-hawaiian-cheese.jpg"
+        };
+        
+
+        //another way to do the same as above, but this is preferable
+        var item2 = new Item
+        {
+            ItemId = 2,
+            Name = "Fried Chicken Leg",
+            Price = 160,
+            Description = "Delicious .....",
+            ImageUrl = "/images/vecteezy_fried-chicken-leg.jpg"
+        };
+
+        var item3 = new Item
+        {
+            ItemId = 3,
+            Name = "Ribs",
+            Price = 260,
+            Description = "Delicious .....",
+            ImageUrl = "/images/ribs.jpg"
+        };
+
+        var item4 = new Item
+        {
+            ItemId = 4,
+            Name = "Fries",
+            Price = 80,
+            Description = "Delicious .....",
+            ImageUrl = "/images/fries.jpg"
+        };
+
+        var item5 = new Item
+        {
+            ItemId = 5,
+            Name = "Fish and Chips",
+            Price = 180,
+            Description = "Delicious .....",
+            ImageUrl = "/images/fish and chips.jpg"
+        };
+
+        var item6 = new Item
+        {
+            ItemId = 6,
+            Name = "Tacos",
+            Price = 180,
+            Description = "Delicious .....",
+            ImageUrl = "/images/vecteezy_ai-generated-tacos.jpg"
+        };
+
+        items.Add(item1);
+        items.Add(item2);
+        items.Add(item3);
+        items.Add(item4);
+        items.Add(item5);
+        items.Add(item6);
+
+
+        ViewBag.CurrentViewName = "List of Shop Items";
+        return items;
+    }
+}
+
+*/
